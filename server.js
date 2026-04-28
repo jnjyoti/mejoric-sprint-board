8162745405cc85a5df85aeaf28ede0337c68007c
jnjyoti <jnjyoti1610@gmail.com>
2026-04-28 20:43:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
